69455c59f721684fa800598d6df65533dbfcc145
ChrissiCl <christina.clemens@web.de>
2020-05-11 15:55:29 +0200

    Neues Themenquiz Früher und Heute

---
+++ scripts/themenraetselfrueherundheute.js
@@ -0,0 +1,71 @@
+var meineDaten =
+{
+	'quize': [
+		{
+			'name': 'Früher und Heute',
+			'abschlussseite': 'raetselende_frueherundheute.html',
+			'raetsel': [
+			{
+				'raetselname': 'Rätsel 1 (Los gehts)',
+				'kategorie': 'Ding',
+				'hinweise': ['Abführen!','Alle schimpfen über mich.','Ich kann erlassen werden.','Mich gibt es in jedem Land.','Ich kann erhoben werden.','Mich gibt es in jedem Auto.','Mein Berater spart Geld und kostet Geld.'],
+				'loesung': ['Steuer','Steuern']
+			},
+			{
+				'raetselname': 'Rätsel 2 (Jahr ist immer doof)',
+				'kategorie': 'Jahr',
+				'hinweise': ['Der Papst pilgert ins Gelobte Land.','Willy Brandt wird Parteivorsitzender.','Tanganjika und Sansibar werden Tansania.','Olympische Spiele finden zum ersten Mal in Tokio statt.','Martin Luther King erhält den Friedensnobelpreis.','Die erste Saison der Fußballbundesliga geht zu Ende mit dem Meister 1. FC Köln','Sehr viele Menschen (Babyboom), darunter viele tolle Menschen, werden geboren.'],
+				'loesung': ['1964']
+			},
+			{
+				'raetselname': 'Rätsel 3 (endlich mal was Einfaches)',
+				'kategorie': 'Ding',
+				'hinweise': ["Wenn man in Österreich von Scheiberln spricht, geht's um mich.",'Ich bestehe aus Fünfecken und Sechsecken.','1878 wurde mein erster deutscher Klub gegründet.','Ich bin ein Spielgerät.','Du darfst mich mit dem Kopf benutzen.','Ich bin rund, hat mal jemand gesagt.','Ich wurde (wahrscheinlich) in England erfunden.'],
+				'loesung': ['Fussball','Fußball']
+			},
+			{
+				'raetselname': 'Rätsel 4 (weiter gehts)',
+				'kategorie': 'Ding',
+				'hinweise': ['Wenn ich noch ein Jahrhundert vor mir habe, schätzt man mich besonders.','Früher wurde ich mit Füßen getreten.','Ich liege im Keller.','Ich bin naß, auch wenn ich trocken bin.','Ich werde aus Früchten gemacht.','Ich liebe die Sonne.','Ich werde gelesen.'],
+				'loesung': ['Wein']
+			},
+			{
+				'raetselname': 'Rätsel 5 (kenne ich nicht?!)',
+				'kategorie': 'Person',
+				'hinweise': ['Ich starb 399 v.Chr. in Athen.','Ich habe nichts Schriftliches hinterlassen.','Ich wurde wegen Gottlosigkeit angeklagt.','Ich habe mein Todesurteil vollstreckt.','Ich bin Grieche.','Mein letzter Drink war Schierling.','Ich weiß, daß ich nichts weiß.'],
+				'loesung': ['Sokrates']
+			},
+			{
+				'raetselname': 'Rätsel 6 (war ich noch nie?!)',
+				'kategorie': 'Ort',
+				'hinweise': ['Mein Autokennzeichen ist NF.','Ich bin 37 Kilometer lang.','Ich habe Lokale aber keine Vokale.','Ich habe einen Ellenbogen.','Ich bin in Schleswig-Holstein.','Ich habe eine ganz typische Form.','Der Hindenburgdamm ist meine Nabelschnur.'],
+				'loesung': ['Sylt']
+			},
+			{
+				'raetselname': 'Rätsel 7 (das kennt jeder)',
+				'kategorie': 'Ding',
+				'hinweise': ['Mein Geschmack ist unterschiedlich.','Ich kann weiß sein.','Manchmal bin ich heiß, meist aber nicht.','Ich werde aus Bohnen gemacht.','Du kannst mich essen oder trinken.','Ich kleide mich gern in Gold oder Silber.','Drückst du mich an dich, dann werde ich weich.'],
+				'loesung': ['Schokolade']
+			},
+			{
+				'raetselname': 'Rätsel 8 (nicht schon wieder ein Jahr...)',
+				'kategorie': 'Jahr',
+				'hinweise': ['Der Entdecker des Penicillins bekommt den Nobelpreis.','Franklin D. Roosevelt stirbt.','Die letzten olympischen Spiele sind schon 9 Jahre her.','Die Stadt Dresden verbrennt.','In Nürnberg beginnt ein Prozeß.','Mein 8. Mai läßt die Welt aufatmen.','Der zweite Weltkrieg ist zu Ende.'],
+				'loesung': ['1945']
+			},
+			{
+				'raetselname': 'Rätsel 9 (oje, ist das lange her)',
+				'kategorie': 'Person',
+				'hinweise': ['Meine große Zeit waren die fünfziger Jahre.','Ich bin Amerikanerin.','Elton John hat mich besungen.','Ich bin Schauspielerin.','Diamanten sind meine besten Freunde.','Ich starb 1962 unter mysteriösen Umständen.','Ich bin ein blonder Männerschwarm.'],
+				'loesung': ['Marilyn Monroe']
+			},
+			{
+				'raetselname': 'Rätsel 10 (gleich ist es geschafft)',
+				'kategorie': 'Ding',
+				'hinweise': ['Ich bin ein Teil von dir.','Mein Sieg kann errungen werden.','Laß mich nicht hängen.','Mein Gelenk ist rundum beweglich.','An mir kann man sich ausweinen.','Auf mir kann viel Verantwortung lasten.','Mein Riemen hält Taschen.'],
+				'loesung': ['Schulter', 'Schultergelenk']
+			}
+			]
+		}
+	]
+};
