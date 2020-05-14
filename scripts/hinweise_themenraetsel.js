bad30d3e31db2f48c508cdc640571f5ee2211af8
ChrissiCl <christina.clemens@web.de>
2020-05-14 17:50:52 +0200

    Norwegenquiz

--- scripts/hinweise_themenraetsel.js
+++ scripts/hinweise_themenraetsel.js
@@ -66,6 +66,138 @@ var meineDaten =
 				'loesung': ['Frühling', 'Frühjahr']
 			}
 			]
+		},
+		{
+			'name': 'Früher und Heute',
+			'abschlussseite': 'raetselende_frueherundheute.html',
+			'raetsel': [
+			{
+				'raetselname': 'Rätsel 1 (Los gehts)',
+				'kategorie': 'Ding',
+				'hinweise': ['Abführen!','Alle schimpfen über mich.','Ich kann erlassen werden.','Mich gibt es in jedem Land.','Ich kann erhoben werden.','Mich gibt es in jedem Auto.','Mein Berater spart Geld und kostet Geld.'],
+				'loesung': ['Steuer','Steuern']
+			},
+			{
+				'raetselname': 'Rätsel 2 (Jahr ist immer doof)',
+				'kategorie': 'Jahr',
+				'hinweise': ['Der Papst pilgert ins Gelobte Land.','Willy Brandt wird Parteivorsitzender.','Tanganjika und Sansibar werden Tansania.','Olympische Spiele finden zum ersten Mal in Tokio statt.','Martin Luther King erhält den Friedensnobelpreis.','Die erste Saison der Fußballbundesliga geht zu Ende mit dem Meister 1. FC Köln','Sehr viele Menschen (Babyboom), darunter viele tolle Menschen, werden geboren.'],
+				'loesung': ['1964']
+			},
+			{
+				'raetselname': 'Rätsel 3 (endlich mal was Einfaches)',
+				'kategorie': 'Ding',
+				'hinweise': ["Wenn man in Österreich von Scheiberln spricht, geht's um mich.",'Ich bestehe aus Fünfecken und Sechsecken.','1878 wurde mein erster deutscher Klub gegründet.','Ich bin ein Spielgerät.','Du darfst mich mit dem Kopf benutzen.','Ich bin rund, hat mal jemand gesagt.','Ich wurde (wahrscheinlich) in England erfunden.'],
+				'loesung': ['Fussball','Fußball']
+			},
+			{
+				'raetselname': 'Rätsel 4 (weiter gehts)',
+				'kategorie': 'Ding',
+				'hinweise': ['Wenn ich noch ein Jahrhundert vor mir habe, schätzt man mich besonders.','Früher wurde ich mit Füßen getreten.','Ich liege im Keller.','Ich bin naß, auch wenn ich trocken bin.','Ich werde aus Früchten gemacht.','Ich liebe die Sonne.','Ich werde gelesen.'],
+				'loesung': ['Wein']
+			},
+			{
+				'raetselname': 'Rätsel 5 (kenne ich nicht?!)',
+				'kategorie': 'Person',
+				'hinweise': ['Ich starb 399 v.Chr. in Athen.','Ich habe nichts Schriftliches hinterlassen.','Ich wurde wegen Gottlosigkeit angeklagt.','Ich habe mein Todesurteil vollstreckt.','Ich bin Grieche.','Mein letzter Drink war Schierling.','Ich weiß, daß ich nichts weiß.'],
+				'loesung': ['Sokrates']
+			},
+			{
+				'raetselname': 'Rätsel 6 (war ich noch nie?!)',
+				'kategorie': 'Ort',
+				'hinweise': ['Mein Autokennzeichen ist NF.','Ich bin 37 Kilometer lang.','Ich habe Lokale aber keine Vokale.','Ich habe einen Ellenbogen.','Ich bin in Schleswig-Holstein.','Ich habe eine ganz typische Form.','Der Hindenburgdamm ist meine Nabelschnur.'],
+				'loesung': ['Sylt']
+			},
+			{
+				'raetselname': 'Rätsel 7 (das kennt jeder)',
+				'kategorie': 'Ding',
+				'hinweise': ['Mein Geschmack ist unterschiedlich.','Ich kann weiß sein.','Manchmal bin ich heiß, meist aber nicht.','Ich werde aus Bohnen gemacht.','Du kannst mich essen oder trinken.','Ich kleide mich gern in Gold oder Silber.','Drückst du mich an dich, dann werde ich weich.'],
+				'loesung': ['Schokolade']
+			},
+			{
+				'raetselname': 'Rätsel 8 (nicht schon wieder ein Jahr...)',
+				'kategorie': 'Jahr',
+				'hinweise': ['Der Entdecker des Penicillins bekommt den Nobelpreis.','Franklin D. Roosevelt stirbt.','Die letzten olympischen Spiele sind schon 9 Jahre her.','Die Stadt Dresden verbrennt.','In Nürnberg beginnt ein Prozeß.','Mein 8. Mai läßt die Welt aufatmen.','Der zweite Weltkrieg ist zu Ende.'],
+				'loesung': ['1945']
+			},
+			{
+				'raetselname': 'Rätsel 9 (oje, ist das lange her)',
+				'kategorie': 'Person',
+				'hinweise': ['Meine große Zeit waren die fünfziger Jahre.','Ich bin Amerikanerin.','Elton John hat mich besungen.','Ich bin Schauspielerin.','Diamanten sind meine besten Freunde.','Ich starb 1962 unter mysteriösen Umständen.','Ich bin ein blonder Männerschwarm.'],
+				'loesung': ['Marilyn Monroe']
+			},
+			{
+				'raetselname': 'Rätsel 10 (gleich ist es geschafft)',
+				'kategorie': 'Ding',
+				'hinweise': ['Ich bin ein Teil von dir.','Mein Sieg kann errungen werden.','Laß mich nicht hängen.','Mein Gelenk ist rundum beweglich.','An mir kann man sich ausweinen.','Auf mir kann viel Verantwortung lasten.','Mein Riemen hält Taschen.'],
+				'loesung': ['Schulter', 'Schultergelenk']
+			}
+			]
+		},
+		{
+			'name': '17. Mai - Norges nasjonaldag',
+			'abschlussseite': 'raetselende_noquiz.html',
+			'raetsel': [
+			{
+				'raetselname': 'Rätsel 1',
+				'kategorie': 'Ding',
+				'hinweise': ['Ich bin ein Überbleibsel der letzten Eiszeit','Ich wurde von Gletschern geformt','Ich bin eine Sehenswürdigkeit','Ich bin U-förmig','Ich bin tief','Man findet mich nicht nur in Norwegen','Ich bin lang'],
+				'loesung': ['Fjord']
+			},
+			{
+				'raetselname': 'Rätsel 2',
+				'kategorie': 'Ding',
+				'hinweise': ['Ich bin ein Rechtsdokument','In der Bundesrepublik Deutschland gibt es mich seit 1949','Ich regele die Grundlagen eines Staates','Ich bin ein Symbol für Demokratie','Zu meinem Schutz gibt es ein Bundesamt','Es gibt ein Gericht, das für meine Auslegung zuständig ist','Bin ich ein Zustand, kann ich gut oder schlecht sein'],
+				'loesung': ['Verfassung']
+			},
+			{
+				'raetselname': 'Rätsel 3',
+				'kategorie': 'Jahr',
+				'hinweise': ['Napoleon Bonaparte muss abdanken','Die norwegische Verfassung wird verabschiedet','Die finale Fassung von Fidelio von Beethoven wird uraufgeführt','Norwegen geht von Dänemark an Schweden','Der Friede von Gent beendet den Britisch-Amerikanischen Krieg','Der Wiener Kongress beginnt','Die Bourbonen kommen in Frankreich wieder an die Macht'],
+				'loesung': ['1814']
+			},
+			{
+				'raetselname': 'Rätsel 4',
+				'kategorie': 'Ort',
+				'hinweise': ['Ich bin die Stadt der sieben Hügel','Ich bin mehrmals abgebrannt','Mit Oslo bin ich über eine der schönsten Eisenbahnstrecken Europas verbunden','Ich war die Heimatstadt Edvard Griegs','Mein Fußballverein ist Feuer und Flamme','Ich bin die regenreichste Stadt Europas','Ich bin das Tor zu den Fjorden'],
+				'loesung': ['Bergen']
+			},
+			{
+				'raetselname': 'Rätsel 5',
+				'kategorie': 'Ding',
+				'hinweise': ['Meistens bin ich rechteckig','Meine Flucht ist ein schweres Vergehen','Soldaten schwören auf mich','Oft bin ich ein Tuch','Mich gibt es in vielen Farben','Zu mir kann man rufen','Am Ende meiner Stange geht es nicht weiter'],
+				'loesung': ['Fahne']
+			},
+			{
+				'raetselname': 'Rätsel 6',
+				'kategorie': 'Person',
+				'hinweise': ['Ich war Apotheker','Ich war mit Bjørnstjerne Bjørnson befreundet','Ich war freiwillig in Italien und Deutschland im Exil','Ich stehe vor dem Nationaltheater in Oslo','Meine Werke sind gesellschaftskritisch','Ich habe das Theater revolutioniert','Ich bin der Verfasser des "nordischen Faust"'],
+				'loesung': ['Henrik Ibsen', 'Ibsen']
+			},
+			{
+				'raetselname': 'Rätsel 7 (das kennt jeder)',
+				'kategorie': 'Ding',
+				'hinweise': ['Ich bin eine Bekleidung','In Deutschland denkt man bei mir vor allem an Bayern','Ich stamme vor allem aus dem 19. Jahrhundert','Ich vermittle eine Vielzahl an Informationen','In Norwegen heiße ich "bunad"','Ich bin ein Identifikationsmittel','Traditionell werde ich aufwändig hergestellt'],
+				'loesung': ['Tracht']
+			},
+			{
+				'raetselname': 'Rätsel 8',
+				'kategorie': 'Ort',
+				'hinweise': ['300 Jahre lang hieß ich "Kristiania/Christiania"','Ost und West sind sehr gegensätzlich','1952 fanden hier olympische Winterspiele statt','Ein Friedensprozess ist nach mir benannt','Ich liege zwischen Fjord und Wald','Einmal im Jahr ist mein Rathaus weltweit in den Nachrichten','Ich bin sehr teuer'],
+				'loesung': ['Oslo']
+			},
+			{
+				'raetselname': 'Rätsel 9',
+				'kategorie': 'Ding',
+				'hinweise': ['Ich bin eine Abwehrmaßnahme','Ich bin eine Präsentation','Ich bin ein Reitelement','Ein Synonym für mich ist "Defilee"','Ich bin ein sehr gutes Beispiel','Ich bin eine Prozession','Ich finde oft an festen Tagen oder Feiertagen statt'],
+				'loesung': ['Parade']
+			},
+			{
+				'raetselname': 'Rätsel 10',
+				'kategorie': 'Ding',
+				'hinweise': ['Ich bin feierlich','Oft stamme ich aus der romantisch nationalistischen Bewegung','Ich bin nicht immer unproblematisch','Meist werde ich gesungen','Ich bin ein Staatssymbol','Ich werde vor allem bei internationalen Sportveranstaltungen genutzt','Über meine Texte wird viel diskutiert'],
+				'loesung': ['Nationalhymne']
+			}
+			]
 		}
 	]
 };
