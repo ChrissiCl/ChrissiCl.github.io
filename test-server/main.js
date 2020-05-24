bb5842f0bae827b6fb85d7e73c702fc21f743f72
ChrissiCl <christina.clemens@web.de>
2020-05-24 16:25:26 +0200

    try https

--- test-server/main.js
+++ test-server/main.js
@@ -2,7 +2,7 @@ var request = new XMLHttpRequest();
 var buttonEins = document.querySelector('button');
 
 buttonEins.onclick = function() {
-	request.open('GET','http://192.168.0.234/phpTest/test.php');
+	request.open('GET','https://192.168.0.234/phpTest/test.php');
 	request.setRequestHeader('X-Test','test1');
 	request.setRequestHeader('X-Test','test2');
 	request.addEventListener('load', function(event) {
