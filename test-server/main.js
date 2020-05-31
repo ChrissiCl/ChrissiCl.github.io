9e33d0dbb85f4a050caf0e9852afcef4d8877706
ChrissiCl <christina.clemens@web.de>
2020-05-31 13:32:59 +0000

    php und httpRequest Tests

--- test-server/main.js
+++ test-server/main.js
@@ -1,16 +1,21 @@
 var request = new XMLHttpRequest();
-var buttonEins = document.querySelector('button');
+var buttonRanking = document.querySelector('#punktebutton');
+var input = document.querySelector('#punktzahl');
+var quizNummer = document.querySelector('#quiznummer');
 
-buttonEins.onclick = function() {
-	request.open('GET','https://192.168.0.234/phpTest/test.php');
-	request.setRequestHeader('X-Test','test1');
-	request.setRequestHeader('X-Test','test2');
+input.setAttribute('value', 45);
+quiznummer.setAttribute('value', 'Quiz 5')
+
+
+buttonRanking.onclick = function() {
+	request.open('POST','test.php');
+	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 	request.addEventListener('load', function(event) {
-	if (request.status >= 200 && request.status < 300) {
-		console.log(request.responseText);
-	} else {
-		console.warn(request.statusText, request.responseText);
-	}
+		if (request.status >= 200 && request.status < 300) {
+			console.log(request.responseText);
+		} else {
+			console.warn(request.statusText, request.responseText);
+		}
 	});
-	request.send();
+	request.send('quizname=' + document.getElementById('quiznummer').value + '&name=' + document.getElementById('name').value + '&punktzahl=' + document.getElementById('punktzahl').value);
 }
---
+++ test-server/test.php
@@ -0,0 +1,49 @@
+<?php
+
+$gesendeteDaten = $_POST;
+//$gesendeteDaten = array("quizname" => "Quiz3", "name" => "Chrissi", "punktzahl" => 70);
+
+$jsonDaten = file_get_contents("test.json");
+$myRanking = json_decode($jsonDaten, True);
+
+$zeile["name"] = $gesendeteDaten["name"];
+$zeile["punktzahl"] = intval($gesendeteDaten["punktzahl"]);
+
+function quizvorhanden($name, $liste){
+	foreach($liste as $key=>$val){
+		if ($name == $val["quizname"]){
+			return True;
+		}
+	}
+	return False;
+}
+
+function quiznummer($name, $liste){
+	$nummer = 0;
+	foreach($liste as $key=>$val){
+		if ($name == $val["quizname"]){
+			return $nummer;
+		}
+		$nummer = $nummer + 1;
+	}
+	return False;
+}
+
+if (quizvorhanden($gesendeteDaten["quizname"], $myRanking["ranking"])){
+	array_push($myRanking["ranking"][quiznummer($gesendeteDaten["quizname"], $myRanking["ranking"])]["user"], $zeile);
+}
+else{
+	$quiz["quizname"] = $gesendeteDaten["quizname"];
+	$quiz["user"] = array($zeile);
+	array_push($myRanking["ranking"], $quiz);
+}
+
+$myJSON = json_encode($myRanking);
+file_put_contents("test.json", $myJSON);
+
+$responseStatus = '200 OK';
+header($_SERVER['SERVER_PROTOCOL'].' '.$responseStatus);
+header('Content-type: text/html; charset=utf-8');
+echo $myJSON;
+
+?>
