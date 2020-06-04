1f5d73b66b486033e9335fb09d759ad231a3edae
ChrissiCl <christina.clemens@web.de>
2020-06-04 17:59:36 +0200

    Test

--- scripts/raetselende.js
+++ scripts/raetselende.js
@@ -24,13 +24,17 @@ input.addEventListener('keyup', function(event) {
   }
 });
 
+function leerzeichenersetzen(textMitLeerzeichen) {
+	return textMitLeerzeichen.replace(/ /g, '+');
+}
+
 buttonRanking.onclick = function() {
 	request.open('POST','scripts/datenspeichern.php');
 	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 	request.addEventListener('load', function(event) {
 		if (request.status >= 200 && request.status < 300) {
 			console.log(request.responseText);
-			document.location.href = 'ranking.php?quizname=' + quizNr;
+			document.location.href = 'ranking.php?quizname=' + leerzeichenersetzen(quizNr);
 		} else {
 			console.warn(request.statusText, request.responseText);
 		}
