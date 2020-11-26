1a4daed5d1f8aa58defa84e5a1949f71b271b853
ChrissiCl <christina.clemens@web.de>
2020-11-26 20:48:36 +0100

    Test

--- scripts/raetselende.js
+++ scripts/raetselende.js
@@ -36,6 +36,7 @@ buttonRanking.onclick = function() {
 			console.log(request.responseText);
 			if (quizNr == 'Quiz zum 1. Advent') {
 				document.location.href = 'ranking.php?quizname=Advent';
+			}
 			// } else {
 				// document.location.href = 'ranking.php?quizname=' + leerzeichenersetzen(quizNr);
 			// }
