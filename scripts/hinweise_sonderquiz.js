ec81136ba1360b3323d9070c0c50b11722af7778
ChrissiCl <christina.clemens@web.de>
2020-11-30 19:28:17 +0100

    Rätsel 2. Advent

--- scripts/hinweise_sonderquiz.js
+++ scripts/hinweise_sonderquiz.js
@@ -12,6 +12,24 @@ var meineDaten =
 				'loesung': ['Rentier']
 			}
 			]
+		},
+		{
+			'name': '2. Advent Quiz',
+			'abschlussseite': 'raetselende_zweiteradvent.shtml',
+			'raetsel': [
+			{
+				'raetselname': 'Schönen zweiten Advent',
+				'kategorie': 'Wird immer noch nicht verraten 😉',
+				'hinweise': ['Es gibt mich 4 Mal', '1926 wurde ich überflogen', '2007 wurde ich mit einem Auto erreicht', 'Ich liege im kleinsten Ozean der Welt', 'Das Meer ist bei mir 4087m tief', 'Ich wandere', 'Mein politischer Status ist unklar'],
+				'loesung': ['Nordpol']
+			},
+			{
+				'raetselname': 'Noch ein Rätsel',
+				'kategorie': 'Was wohl?',
+				'hinweise': ['Ich bin ein Gegenstand von Spekulationen', 'Ich bestehe aus Gas', 'Es gibt sehr viele von mir', 'Ich durchlaufe verschiedene Lebensphasen', 'Man kann mich lesen', 'Ich habe Ecken und Kanten oder bin eine Kugel', 'Zwerg oder Riese'],
+				'loesung': ['Stern']
+			}
+			]
 		}		
 	]
 };
