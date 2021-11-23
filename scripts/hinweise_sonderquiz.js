355b9e300bdcf36423073e8f81d698ca327127a6
ChrissiCl <christina.clemens@web.de>
2021-11-23 20:42:35 +0100

    Adventsrätsel

--- scripts/hinweise_sonderquiz.js
+++ scripts/hinweise_sonderquiz.js
@@ -60,7 +60,7 @@ var meineDaten =
 			'abschlussseite': 'raetselende_vierteradvent.shtml',
 			'raetsel': [
 			{
-				'raetselname': 'Schönen vierter Advent',
+				'raetselname': 'Schönen vierten Advent',
 				'kategorie': 'Mal sehen 😉',
 				'hinweise': ['Ich bin struppig', 'Man findet mich im Stall', 'Ich bin trocken', 'Niemand will wie ich sein', 'Ich bin ein Überbleibsel', 'Manche Sterne bestehen aus mir', 'Ich bin blond'],
 				'loesung': ['Stroh']
@@ -80,6 +80,90 @@ var meineDaten =
 			{
 				'raetselname': '3. Rätsel',
 				'kategorie': '?',
+				'hinweise': ['Ich bin klein', 'Tomte Tummetott', 'Ich lebe versteckt', 'Ich tue Gutes', 'In Skandinavien helfe ich dem Weihnachtsmann', 'Die Maskottchen des ZDF gehören zu meiner Art', 'Ich bin ein Diminutiv'],
+				'loesung': ['Wichtel']
+			}
+			]
+		},
+		{
+			'name': '1. Advent Quiz',
+			'abschlussseite': 'raetselende_ersteradvent2.shtml',
+			'raetsel': [
+			{
+				'raetselname': 'Schönen ersten Advent',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Ich bin eine geometrische Form', 'Ich bin eine runde Sache', 'Ich bin ein Geschoss', 'Mit Rum bin ich eine Süßigkeit', 'Ich beschäftige Mathematiker', 'Ich bin ein Symbol für Unendlichkeit', 'Ich gelte als vollkommen'],
+				'loesung': ['Kugel']
+			}
+			]
+		},
+		{
+			'name': '2. Advent Quiz',
+			'abschlussseite': 'raetselende_zweiteradvent2.shtml',
+			'raetsel': [
+			{
+				'raetselname': 'Schönen zweiten Advent',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Es gibt mich 4 Mal', '1926 wurde ich überflogen', '2007 wurde ich mit einem Auto erreicht', 'Ich liege im kleinsten Ozean der Welt', 'Das Meer ist bei mir 4087m tief', 'Ich wandere', 'Mein politischer Status ist unklar'],
+				'loesung': ['Nordpol']
+			},
+			{
+				'raetselname': 'Noch ein Rätsel',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Ich bin ein Gegenstand von Spekulationen', 'Ich bestehe aus Gas', 'Es gibt sehr viele von mir', 'Ich durchlaufe verschiedene Lebensphasen', 'Man kann mich lesen', 'Ich habe Ecken und Kanten oder bin eine Kugel', 'Zwerg oder Riese'],
+				'loesung': ['Stern']
+			}
+			]
+		},
+		{
+			'name': '3. Advent Quiz',
+			'abschlussseite': 'raetselende_dritteradvent2.shtml',
+			'raetsel': [
+			{
+				'raetselname': 'Schönen dritten Advent',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Ich bin ein Bühnenstück', 'Ich bin ein Werkzeug', 'Es gibt mich in verschiedenen Varianten', 'Ich bin eine Holzfigur', 'Ich kann funktional oder dekorativ sein', 'Ich komme in einem Märchen vor', 'Ist mein Gegner eine Nuss oder eine Maus?'],
+				'loesung': ['Nussknacker']
+			},
+			{
+				'raetselname': '2. Rätsel',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Ich bin offen oder geschlossen', 'Ich bin ein enger Felsspalt', 'Mit Pulver kann man durch mich reisen', 'Ich habe eigenes Besteck', 'Ich bin entspannend', 'Heutzutage bin ich vor allem dekorativ', 'Ich habe ein Sims'],
+				'loesung': ['Kamin']
+			},
+			{
+				'raetselname': '3. Rätsel',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Ich diene dem Transport', 'Ich bin ein Musikant', 'Ich kann Gold spucken', 'Ich bin eigensinnig', 'Ich gelte als nicht besonders schlau', 'Mit einer Brücke bin ich hilfreich', 'Mein Name ist "Equus asinus"'],
+				'loesung': ['Esel']
+			}
+			]
+		},
+		{
+			'name': '4. Advent Quiz',
+			'abschlussseite': 'raetselende_vierteradvent2.shtml',
+			'raetsel': [
+			{
+				'raetselname': 'Schönen vierten Advent',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Ich bin struppig', 'Man findet mich im Stall', 'Ich bin trocken', 'Niemand will wie ich sein', 'Ich bin ein Überbleibsel', 'Manche Sterne bestehen aus mir', 'Ich bin blond'],
+				'loesung': ['Stroh']
+			},
+			{
+				'raetselname': '2. Rätsel',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Ich bin dunkel', 'Ich bin ein Violinkonzert von Vivaldi', 'Ich kann tief sein', 'In der Meteorologie beginne ich früher', 'Mein Name ist ein germanisches Sonderwort', 'Ich lasse vieles schlafen', 'Manchmal bin ich hart'],
+				'loesung': ['Winter']
+			},
+			{
+				'raetselname': '3. Rätsel',
+				'kategorie': 'Alles ist möglich',
+				'hinweise': ['Ich bin eine Institution', 'Ich bin diplomatisch', 'Ich kann gut oder schlecht sein', 'Ich kann aus Papier sein', 'Meine Überbringer können vielgestaltig sein', '1974 ging ich an Außerirdische', 'Man findet mich in Hauptstädten'],
+				'loesung': ['Botschaft']
+			},
+			{
+				'raetselname': '3. Rätsel',
+				'kategorie': 'Alles ist möglich',
 				'hinweise': ['Ich bin klein', 'Tomte Tummetott', 'Ich lebe versteckt', 'Ich tue Gutes', 'In Skandinavien helfe ich dem Weihnachtsmann', 'Die Maskottchen des ZDF gehören zu meiner Art', 'Ich bin ein Diminutiv'],
 				'loesung': ['Wichtel']
 			}
--- scripts/index.js
+++ scripts/index.js
@@ -8,51 +8,51 @@ var kerze2 = document.getElementById('Kerze_2')
 var kerze3 = document.getElementById('Kerze_3')
 var kerze4 = document.getElementById('Kerze_4')
 
-// window.onload = function() {
-	// // if (now >= 1605975437000) {
-		// // kerze1.src = 'images/Kerze_mit_Flamme.png';
-		// // kerze1.alt = 'Kerze mit Flamme';
-	// // }
-	// if (now >= 1606608060000) {
+window.onload = function() {
+	// if (now >= 1605975437000) {
 		// kerze1.src = 'images/Kerze_mit_Flamme.png';
+		// kerze1.alt = 'Kerze mit Flamme';
 	// }
-	// if (now >= 1607212860000) {
-		// kerze2.src = 'images/Kerze_mit_Flamme.png';
-	// }
-	// if (now >= 1607817660000) {
-		// kerze3.src = 'images/Kerze_mit_Flamme.png';
-	// }
-	// if (now >= 1608422460000) {
-		// kerze4.src = 'images/Kerze_mit_Flamme.png';
-	// }
-// }
+	if (now >= 1638057660000) {
+		kerze1.src = 'images/Kerze_mit_Flamme.png';
+	}
+	if (now >= 1638662460000) {
+		kerze2.src = 'images/Kerze_mit_Flamme.png';
+	}
+	if (now >= 1639267260000) {
+		kerze3.src = 'images/Kerze_mit_Flamme.png';
+	}
+	if (now >= 1608422460000) {
+		kerze4.src = 'images/Kerze_mit_Flamme.png';
+	}
+}
 
-// kerze1.onclick = function() {
-	// // if (now >= 1605975197000) {
-		// // document.location.href = 'quiz_ersteradvent.shtml';
-	// // }
-	// if (now >= 1606608060000) {
+kerze1.onclick = function() {
+	// if (now >= 1605975197000) {
 		// document.location.href = 'quiz_ersteradvent.shtml';
 	// }
-// }
+	if (now >= 1638057660000) {
+		document.location.href = 'quiz_ersteradvent2.shtml';
+	}
+}
 
-// kerze2.onclick = function() {
-	// if (now >= 1607212860000) {
-		// document.location.href = 'quiz_zweiteradvent.shtml';
-	// }
-// }
+kerze2.onclick = function() {
+	if (now >= 1638662460000) {
+		document.location.href = 'quiz_zweiteradvent2.shtml';
+	}
+}
 
-// kerze3.onclick = function() {
-	// if (now >= 1607817660000) {
-		// document.location.href = 'quiz_dritteradvent.shtml';
-	// }
-// }
+kerze3.onclick = function() {
+	if (now >= 1639267260000) {
+		document.location.href = 'quiz_dritteradvent2.shtml';
+	}
+}
 
-// kerze4.onclick = function() {
-	// if (now >= 1608422460000) {
-		// document.location.href = 'quiz_vierteradvent.shtml';
-	// }
-// }
+kerze4.onclick = function() {
+	if (now >= 1608422460000) {
+		document.location.href = 'quiz_vierteradvent2.shtml';
+	}
+}
 
 quiz1Button.onclick = function() {
 	document.location.href = 'quiz1.shtml';
